7f5a1d8330298627d16b4ca7ab654f7efd42b82e
Liu Siyuan <weifengzi2009@gmail.com>
2016-05-29 19:59:24 +0800

    implement Adams

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -145,7 +145,24 @@ def adam(x, dx, config=None):
   # the next_x variable. Don't forget to update the m, v, and t variables     #
   # stored in config.                                                         #
   #############################################################################
-  pass
+  lr_rate = config["learning_rate"]
+  beta1 = config["beta1"]
+  beta2 = config["beta2"]
+  eps = config["epsilon"]
+  m = config["m"]
+  v = config["v"]
+  t = config["t"]
+  # update x
+  t += 1
+  m = beta1 * m + (1 - beta1) * dx
+  v = beta2 * v + (1 - beta2) * np.square(dx)
+  m_corrected = m / (1 - (beta1 ** t))
+  v_corrected = v / (1 - (beta2 ** t))
+  next_x = x - lr_rate * m_corrected / (np.sqrt(v_corrected) + eps)
+  # update variables
+  config["m"] = m
+  config["v"] = v
+  config["t"] = t
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
